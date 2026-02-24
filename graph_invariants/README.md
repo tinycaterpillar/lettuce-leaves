205af922058c62a7b4535b2355fcc237cde1d549
jinik <jinik990305@gmail.com>
2026-02-24 20:56:09 +0900

    .

--- graph_invariants/README.md
+++ graph_invariants/README.md
@@ -3,3 +3,4 @@ nohup python main.py data/11to20 &
 nohup python main.py data/21to30 &
 nohup python main.py data/31to70 &
 nohup python main.py data/71toinf &
+
--- graph_invariants/main.py
+++ graph_invariants/main.py
@@ -5,13 +5,13 @@ import random
 import os
 
 from utils import draw, setup_logger
-from solvers import decode_oriented_path, find_subgraph_with_vertex, make_oriented_path
+from solvers import decode_oriented_path, find_subgraph_isomorphism, make_oriented_path
 
 if __name__ == "__main__":
     logger = setup_logger(folder="log", filename=f"log{os.getpid()}.log")
 
-    k = random.randint(3, 6)
-    n = random.randint(2*k+1, 2*k+5)
+    k = random.randint(3, 4)
+    n = random.randint(2*k+1, 2*k+10)
     seed = random.randint(1, 1000)
     length = 2*k-1
     logger.info(f"k {k}, n {n}, seed {seed}")
@@ -19,6 +19,8 @@ if __name__ == "__main__":
     
     D = graphs.RandomRegular(2*k, n, seed=seed).eulerian_orientation()
     for op in range(1<<length):
-        p = find_subgraph_with_vertex(D, make_oriented_path(op, length), 0).__next__()
-        draw(D, p, name=decode_oriented_path(op, length), folder=f"results/{n}_{k}_{seed}")
+        logger.log(f"op {decode_oriented_path(op, length)}, {len(list(find_subgraph_isomorphism(D, make_oriented_path(op, length))))}")
+        # p = find_subgraph_with_vertex(D, make_oriented_path(op, length), 0).__next__()
+        # draw(D, p, name=decode_oriented_path(op, length), folder=f"results/{n}_{k}_{seed}")
+
     
